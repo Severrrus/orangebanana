e97203e8ca97effdd69233aad778734211c843f7
Mateusz Nowak <mtsznowak9@gmail.com>
2015-04-22 17:08:00 +0200

    comm

--- ChickenTime/src/com/example/chickentime/MainActivity.java
+++ ChickenTime/src/com/example/chickentime/MainActivity.java
@@ -1,22 +1,53 @@
 package com.example.chickentime;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface.OnShowListener;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
 import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
+  static MainActivity main;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+    main = this;
     setContentView(R.layout.activity_main);
-   
   }
+
+  @Override
+  public void onPause()
+  {
+    super.onPause(); // Always call the superclass method first
+    kill();
+    
+    SharedPreferences sharedPref= this.getSharedPreferences("com.example.chickentime", Context.MODE_PRIVATE);
+    sharedPref.edit().putBoolean("chickenIsAlive", isChickenAlive()).apply();
   
-  public void kill(View view){
-    Toast aToast = Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT);
+  }
+
+  public void kill()
+  {
+    new Handler().postDelayed(new Runnable()
+    {
+
+      @Override
+      public void run()
+      {
+        Toast aToast = Toast.makeText(MainActivity.main,getString(R.string.ChickenKilled) , Toast.LENGTH_SHORT);
+        aToast.show();
+      }
+    }, 2000);
+
+  }
+
+  private boolean isChickenAlive()
+  {
+    return false;
   }
 }
