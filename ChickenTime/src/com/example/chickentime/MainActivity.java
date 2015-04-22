b8c80b214c7efba14a5c4d26e05f80f0ef1c1e5a
Mateusz Nowak <mtsznowak9@gmail.com>
2015-04-22 18:28:16 +0200

    Activities

--- ChickenTime/src/com/example/chickentime/MainActivity.java
+++ ChickenTime/src/com/example/chickentime/MainActivity.java
@@ -1,22 +1,27 @@
 package com.example.chickentime;
 
+import com.example.chickentime.Chicken.ChickenStatus;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface.OnShowListener;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
   static MainActivity main;
-
+  Chicken chicken;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     main = this;
+    chicken = new Chicken();
     setContentView(R.layout.activity_main);
     onResume();
   }
@@ -28,7 +33,9 @@ public class MainActivity extends Activity
     SharedPreferences sharedPref= this.getSharedPreferences("com.example.chickentime", Context.MODE_PRIVATE);
     if (sharedPref.contains("chickenIsAlive")){
       if(!sharedPref.getBoolean("chickenIsAlive", true)){
-        
+        sharedPref.edit().putBoolean("chickenIsAlive", true).apply();
+        Intent intent = new Intent(this, KillChickenActivity.class);
+        //startActivity(intent);
       }
     }
   }
@@ -37,11 +44,17 @@ public class MainActivity extends Activity
   public void onPause()
   {
     super.onPause(); // Always call the superclass method first
-    kill();
+    if (isChickenAlive())
+      kill();
     
     SharedPreferences sharedPref= this.getSharedPreferences("com.example.chickentime", Context.MODE_PRIVATE);
     sharedPref.edit().putBoolean("chickenIsAlive", isChickenAlive()).apply();
   
+  }
+  
+  public void farm(){
+    Intent intent = new Intent(this, ListViewChickens.class);
+    startActivity(intent);
   }
 
   public void kill()
@@ -59,8 +72,22 @@ public class MainActivity extends Activity
 
   }
 
+  public void spawnChicken(View view){
+    chicken.status = Chicken.ChickenStatus.DOSTHG;
+    findViewById(R.id.button1).setVisibility(View.INVISIBLE);
+    findViewById(R.id.button2).setVisibility(View.VISIBLE);
+  }
+  
+  public void restartChicken(View view){
+    chicken.status = Chicken.ChickenStatus.START;
+    findViewById(R.id.button2).setVisibility(View.INVISIBLE);
+    findViewById(R.id.button1).setVisibility(View.VISIBLE);
+  }
+  
   private boolean isChickenAlive()
   {
-    return false;
+    if (chicken.status == ChickenStatus.START)
+      return false;
+    return true;
   }
 }
