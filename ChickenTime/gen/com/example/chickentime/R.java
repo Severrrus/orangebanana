5201d6ea4bb6651e17b8a241ee1747b3b0610fb8
mpluta <m.pluta.real@interia.pl>
2015-04-22 20:35:01 +0200

    Some views for Farm

--- ChickenTime/gen/com/example/chickentime/R.java
+++ ChickenTime/gen/com/example/chickentime/R.java
@@ -14,27 +14,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int button1=0x7f060000;
-        public static final int listViewChickens=0x7f060001;
-=======
         public static final int button1=0x7f060003;
         public static final int button2=0x7f060002;
         public static final int button3=0x7f060004;
         public static final int editText1=0x7f060001;
         public static final int imageView1=0x7f060000;
->>>>>>> b8c80b214c7efba14a5c4d26e05f80f0ef1c1e5a
+        public static final int itemChickenElapsedTime=0x7f060008;
+        public static final int itemChickenRemainingTime=0x7f060009;
+        public static final int itemChickenStart=0x7f060007;
+        public static final int linearLayoutChicken=0x7f060006;
+        public static final int listViewChickens=0x7f060005;
     }
     public static final class layout {
         public static final int activity_kill_chicken=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int farm_layout=0x7f030002;
+        public static final int item_chicken=0x7f030003;
     }
     public static final class string {
         public static final int ChickenKilled=0x7f040002;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int title_activity_kill_chicken=0x7f040003;
+        public static final int title_list_view_chickens=0x7f040004;
     }
     public static final class style {
         /** 
--- ChickenTime/src/com/example/chickentime/ListViewChickens.java
+++ ChickenTime/src/com/example/chickentime/ListViewChickens.java
@@ -1,5 +1,9 @@
 package com.example.chickentime;
 
+import models.ChickenListViewModel;
+
+import com.example.helpers.ListViewChickensAdapter;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,15 +25,58 @@ public class ListViewChickens extends Activity {
         listView = (ListView) findViewById(R.id.listViewChickens);
         
         // Defined Array values to show in ListView
-        String[] values = new String[] { "11:20:20 11:20:20 11:20:20", 
-        		"2:00:00 1:20:01 0:40:00",
-        		"11:20:20 11:20:20 11:20:20",
-        		"11:20:20 11:20:20 11:20:20", 
-        		"11:20:20 11:20:20 11:20:20", 
-        		"11:20:20 11:20:20 11:20:20", 
-        		"11:20:20 11:20:20 11:20:20", 
-        		"11:20:20 11:20:20 11:20:20" 
-                                        };
+        ChickenListViewModel chicken1 = new ChickenListViewModel();
+        ChickenListViewModel chicken2 = new ChickenListViewModel();
+        ChickenListViewModel chicken3 = new ChickenListViewModel();
+        ChickenListViewModel chicken4 = new ChickenListViewModel();
+        ChickenListViewModel chicken5 = new ChickenListViewModel();
+        ChickenListViewModel chicken6 = new ChickenListViewModel();
+        ChickenListViewModel chicken7 = new ChickenListViewModel();
+        ChickenListViewModel chicken8 = new ChickenListViewModel();
+        ChickenListViewModel chicken9 = new ChickenListViewModel();
+        ChickenListViewModel chicken10 = new ChickenListViewModel();
+        
+        chicken1.setStart("2:00:00");
+        chicken1.setElapsedTime("1:20:00");
+        chicken1.setRemainingTime("0:40:00");
+        chicken1.setSurvived(false);
+        chicken2.setStart("0:20:00");
+        chicken2.setElapsedTime("0:20:00");
+        chicken2.setRemainingTime("0:00:00");
+        chicken2.setSurvived(true);
+        chicken3.setStart("0:45:00");
+        chicken3.setElapsedTime("0:45:00");
+        chicken3.setRemainingTime("0:00:00");
+        chicken3.setSurvived(true);
+        chicken4.setStart("2:00:00");
+        chicken4.setElapsedTime("1:20:00");
+        chicken4.setRemainingTime("0:40:00");
+        chicken4.setSurvived(false);
+        chicken5.setStart("1:30:00");
+        chicken5.setElapsedTime("1:20:00");
+        chicken5.setRemainingTime("0:10:00");
+        chicken5.setSurvived(false);
+        chicken6.setStart("2:00:00");
+        chicken6.setElapsedTime("1:20:00");
+        chicken6.setRemainingTime("0:40:00");
+        chicken6.setSurvived(false);
+        chicken7.setStart("0:20:00");
+        chicken7.setElapsedTime("0:20:00");
+        chicken7.setRemainingTime("0:00:00");
+        chicken7.setSurvived(true);
+        chicken8.setStart("0:45:00");
+        chicken8.setElapsedTime("0:45:00");
+        chicken8.setRemainingTime("0:00:00");
+        chicken8.setSurvived(true);
+        chicken9.setStart("2:00:00");
+        chicken9.setElapsedTime("1:20:00");
+        chicken9.setRemainingTime("0:40:00");
+        chicken9.setSurvived(false);
+        chicken10.setStart("1:30:00");
+        chicken10.setElapsedTime("1:20:00");
+        chicken10.setRemainingTime("0:10:00");
+        chicken10.setSurvived(false);
+        ChickenListViewModel[] values = new ChickenListViewModel[] { chicken1, chicken2, chicken3, chicken4, chicken5, chicken6, chicken7 , chicken8, chicken9, chicken10};
 
         // Define a new Adapter
         // First parameter - Context
@@ -37,34 +84,21 @@ public class ListViewChickens extends Activity {
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-          android.R.layout.simple_list_item_1, android.R.id.text1, values);
+     
 
 
         // Assign adapter to ListView
-        listView.setAdapter(adapter); 
+        listView.setAdapter(new ListViewChickensAdapter(this, values));
         
         // ListView Item Click Listener
-        listView.setOnItemClickListener(new OnItemClickListener() {
+        listView.setOnItemClickListener(new OnItemClickListener(){
 
-              @Override
-              public void onItemClick(AdapterView<?> parent, View view,
-                 int position, long id) {
-                
-               // ListView Clicked item index
-               int itemPosition     = position;
-               
-               // ListView Clicked item value
-               String  itemValue    = (String) listView.getItemAtPosition(position);
-                  
-                // Show Alert 
-                Toast.makeText(getApplicationContext(),
-                  "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-                  .show();
-             
-              }
-
-         }); 
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				
+			}}); 
     }
 
 }
