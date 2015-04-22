bbd6235b75d6a2421d579af5d2efba00542e6ba7
Mateusz Nowak <mtsznowak9@gmail.com>
2015-04-22 17:12:19 +0200

    deleted files

--- ChickenTime/gen/com/example/chickentime/R.java
+++ ChickenTime/gen/com/example/chickentime/R.java
@@ -13,14 +13,12 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
-    public static final class id {
-        public static final int button1=0x7f060000;
-    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int farm_layout=0x7f030001;
     }
     public static final class string {
+        public static final int ChickenKilled=0x7f040002;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
     }
