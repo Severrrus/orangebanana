747f32c6d3645341877ab543355e7adaa8aa8967
Mateusz Nowak <mtsznowak9@gmail.com>
2015-04-23 12:37:15 +0200

    Added chicken activity

---
+++ ChickenTime/src/com/example/chickentime/ChickenView.java
@@ -0,0 +1,17 @@
+package com.example.chickentime;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChickenView extends Activity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_chicken_view);
+  }
+}
