c46f4a18480434dbaa5d4cca83db8972e808c495
mpluta <m.pluta.real@interia.pl>
2015-04-23 18:42:41 +0200

    Upgraded Views in Slider

--- ChickenTime/src/com/example/chickentime/Fragments/TabFragmentStrongestChicken.java
+++ ChickenTime/src/com/example/chickentime/Fragments/TabFragmentStrongestChicken.java
@@ -1,11 +1,13 @@
 package com.example.chickentime.Fragments;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.chickentime.R;
 
@@ -17,6 +19,14 @@ public class TabFragmentStrongestChicken extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.tab_strongest_chicken, container, false);
 
+        Typeface myTypeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/HelveticaNeue.dfont");
+        TextView textViewStrongest = (TextView) rootView.findViewById(R.id.strongest);
+        TextView textViewChicken = (TextView) rootView.findViewById(R.id.chicken);
+        TextView textViewTimeSaved = (TextView) rootView.findViewById(R.id.timeSaved);
+        textViewStrongest.setTypeface(myTypeface);
+        textViewChicken.setTypeface(myTypeface);
+        textViewTimeSaved.setTypeface(myTypeface);
+        
         return rootView;
     }
 }
--- ChickenTime/src/com/example/chickentime/Fragments/TabFragmentTotalChickens.java
+++ ChickenTime/src/com/example/chickentime/Fragments/TabFragmentTotalChickens.java
@@ -1,11 +1,13 @@
 package com.example.chickentime.Fragments;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.chickentime.R;
 
@@ -17,6 +19,14 @@ public class TabFragmentTotalChickens extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.tab_total_chickens, container, false);
 
+        Typeface myTypeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/HelveticaNeue.dfont");
+        TextView textViewTotal = (TextView) rootView.findViewById(R.id.total);
+        TextView textViewChickens = (TextView) rootView.findViewById(R.id.chickens);
+        TextView textViewTotalAmount = (TextView) rootView.findViewById(R.id.totalAmount);
+        textViewTotal.setTypeface(myTypeface);
+        textViewChickens.setTypeface(myTypeface);
+        textViewTotalAmount.setTypeface(myTypeface);
+        
         return rootView;
     }
 }
